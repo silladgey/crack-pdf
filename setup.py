383a1a6d5ae71aae58d85ca0ee13d400889c64e4
Richard Szilagyi <szprichard@proton.me>
2024-08-26 23:31:36 +0100

    chore(workflow): Update GitHub Actions workflow

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
     setup(
         name="crack-pdf",
-        version="0.1.0",
+        version="0.1.1",
         author="Richard Szilagyi",
         author_email="szprichard@proton.me",
         description="A tool to crack password-protected PDFs",
