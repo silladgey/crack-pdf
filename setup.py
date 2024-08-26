0d0e036aea1f065a7691d7287199fa4ef8395bf9
Richard Szilagyi <szprichard@proton.me>
2024-08-26 23:07:34 +0100

    build(setup): Modified setup.py with additional metadata and long description

--- setup.py
+++ setup.py
@@ -1,15 +1,30 @@
 from setuptools import setup, find_packages
 
-setup(
-    name="crack-pdf",
-    version="0.1",
-    packages=find_packages(),
-    install_requires=[
-        "PyPDF2",
-    ],
-    entry_points={
-        "console_scripts": [
-            "crack-pdf=crack_pdf.core:main",
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+    setup(
+        name="crack-pdf",
+        version="0.1.0",
+        author="Richard Szilagyi",
+        author_email="szprichard@proton.me",
+        description="A tool to crack password-protected PDFs",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/szpatrichard/crack-pdf",
+        packages=find_packages(),
+        install_requires=[
+            "PyPDF2",
         ],
-    },
-)
+        entry_points={
+            "console_scripts": [
+                "crack-pdf=crack_pdf.core:main",
+            ],
+        },
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        python_requires=">=3.6",
+    )
